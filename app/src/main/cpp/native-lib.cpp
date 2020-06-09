11df95290dfb7b9c6ffb20267d356794a000bb14
curtes_malteser <antonio_bastiao@hotmail.com>
2020-06-09 23:51:46 +0200

    first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,19 @@
+#include <jni.h>
+#include <string>
+#include "base64.cpp"
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_curtesmalter_myapplication_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_curtesmalter_myapplication_MainActivity_base64FromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = base64_decode("QW50w7NuaW8gQmFzdGnDo28=", true);
+    return env->NewStringUTF(hello.c_str());
+}
